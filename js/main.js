c23f583f426167f0131cd894884254a390914815
Louis Zhang <Louisgeemod@gmail.com>
2022-11-01 17:34:32 +0000

    completed create an entry

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 var $photoUrl = document.querySelector('#PhotoURL');
 var $userIMG = document.querySelector('#user-image');
+var $Title = document.querySelector('#Title');
+var $Notes = document.querySelector('#Notes');
+var $submit = document.querySelector('#submit');
+
+var url = $photoUrl.addEventListener('input', function (event) {
+  url = event.target.value;
+});
+var title = $Title.addEventListener('input', function (event) {
+  title = event.target.value;
+});
+var notes = $Notes.addEventListener('input', function (event) {
+  notes = event.target.value;
+});
 
 $photoUrl.addEventListener('input', function (event) {
   $userIMG.setAttribute('src', event.target.value);
@@ -8,20 +21,6 @@ $photoUrl.addEventListener('input', function (event) {
   }
 }
 );
-
-var url = $photoUrl.addEventListener('input', function (event) {
-  url = event.target.value;
-});
-
-var $Title = document.querySelector('#Title');
-var title = $Title.addEventListener('input', function (event) {
-  title = event.target.value;
-});
-
-var $Notes = document.querySelector('#Notes');
-var notes = $Notes.addEventListener('input', function (event) {
-  notes = event.target.value;
-});
 
 function submitNote(event) {
   var userNote = {
@@ -34,5 +33,10 @@ function submitNote(event) {
   event.preventDefault();
 }
 
-var $submit = document.querySelector('#submit');
+function string(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
+
 $submit.addEventListener('click', submitNote);
+$submit.addEventListener('click', string);
