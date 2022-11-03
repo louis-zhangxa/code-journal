5a21f42222cb66d3d4fd2d0a2ee405ae09bbcd39
Louis Zhang <Louisgeemod@gmail.com>
2022-11-03 02:13:27 +0000

    Remove the example HTML entries once the user's entries are successfully being appended to the DOM on page load.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var $userIMG = document.querySelector('#user-image');
 var $Title = document.querySelector('#Title');
 var $Notes = document.querySelector('#Notes');
 var $submit = document.querySelector('form');
+var $main = document.querySelector('main');
 
 $photoUrl.addEventListener('input', function (event) {
   $userIMG.setAttribute('src', event.target.value);
@@ -26,48 +27,52 @@ function submitNote(event) {
   $submit.reset();
 }
 
-// function renderEntries(data) {
-//   var $container = document.createElement('div');
-//   $container.setAttribute('data-view', 'entries');
-//   $container.setAttribute('class', 'container entries');
+function renderEntries(data) {
+  var $container = document.createElement('div');
+  $container.setAttribute('data-view', 'entries');
+  $container.setAttribute('class', 'container entries');
 
-//   var $row = document.createElement('div');
-//   $row.setAttribute('class', 'row');
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
 
-//   var $columnHalfImg = document.createElement('div');
-//   $columnHalfImg.setAttribute('class', 'column-half');
+  var $columnHalfImg = document.createElement('div');
+  $columnHalfImg.setAttribute('class', 'column-half');
 
-//   var $img = document.createElement('img');
-//   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
-//   $img.setAttribute('alt', 'placeholder');
+  var $img = document.createElement('img');
+  $img.setAttribute('src', data.PhotoUrl);
+  $img.setAttribute('alt', 'placeholder');
 
-//   var $ul = document.createElement('ul');
-//   $ul.setAttribute('class', 'column-half list-text');
+  var $ul = document.createElement('ul');
+  $ul.setAttribute('class', 'column-half list-text');
 
-//   var $liH2 = document.createElement('li');
+  var $liH2 = document.createElement('li');
 
-//   var $h2 = document.createElement('h2');
-//   $h2.textContent = 'Ada Lovelace';
+  var $h2 = document.createElement('h2');
+  $h2.textContent = data.Title;
 
-//   var $liTitle = document.createElement('li');
+  var $li = document.createElement('li');
 
-//   var $pTitle = document.createElement('p');
-//   $pTitle.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus commodi veniam, quas facere dignissimos a repellendus ducimus provident reiciendis ea perferendis obcaecati excepturi amet sed labore. Perferendis, commodi voluptas vitae praesentium at earum provident, consectetur aspernatur unde culpa quia corrupti magni rerum ad, deleniti iure!';
+  var $p = document.createElement('p');
+  $p.textContent = data.Notes;
 
-//   var $liNotes = document.createElement('li');
-//   var $pNotes = document.createElement('p');
-//   $pNotes.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus commodi veniam, quas facere dignissimos a repellendus ducimus provident reiciendis ea perferendis obcaecati excepturi amet sed labore. Perferendis, commodi voluptas vitae praesentium at earum provident, consectetur aspernatur unde culpa quia corrupti magni rerum ad, deleniti iure!';
+  $container.appendChild($row);
+  $row.appendChild($columnHalfImg);
+  $columnHalfImg.appendChild($img);
+  $row.appendChild($ul);
+  $ul.appendChild($liH2);
+  $liH2.appendChild($h2);
+  $ul.appendChild($li);
+  $li.appendChild($p);
 
-//   $container.appendChild($row);
-//   $row.appendChild($columnHalfImg);
-//   $columnHalfImg.appendChild($img);
-//   $row.appendChild($ul);
-//   $ul.appendChild($liH2);
-//   $liH2.appendChild($h2);
-//   $ul.appendChild($liTitle);
-//   $liTitle.appendChild($pTitle);
-//   $ul.appendChild($liNotes);
-//   $liNotes.appendChild($pNotes);
-// }
+  return $container;
+  // console.log($container);
+}
 
+function appendToPage(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    $main.appendChild(renderEntries(data.entries[i]));
+  }
+}
+
+window.addEventListener('DOMContentLoaded', appendToPage);
 $submit.addEventListener('submit', submitNote);
